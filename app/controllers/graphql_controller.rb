f5106e938549f5361f2d254f5950590c48e9acf3
abgata20000 <abgata@abgata.jp>
2021-10-21 13:36:51 +0900

    add: error on production

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -16,7 +16,7 @@ class GraphqlController < ApplicationController
     result = GraphRailsSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue StandardError => e
-    raise e unless Rails.env.development?
+    # raise e unless Rails.env.development?
 
     handle_error_in_development(e)
   end
